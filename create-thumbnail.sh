3d1535b6bb2dfd6893cc4477835ccb295ddf0bc9
sgoryachkin <artful.tiger@gmail.com>
2017-06-06 00:11:46 +0300

    Update create-thumbnail.sh

--- create-thumbnail.sh
+++ create-thumbnail.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+media_dir="$( dirname "$0" )";
+echo "Media dir: $media_dir"
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-for video_file in $(find `pwd` -type f -iname "*.mov" -o -iname "*.avi" -o -iname "*.mkv");
+for video_file in $(find "${media_dir}" -type f -iname "*.mov" -o -iname "*.avi" -o -iname "*.mkv");
 do
-    thumbnail_file=${video_file}.jpg;
-    #echo "${video_file}"
+    thumbnail_file=${video_file}.cover.jpg;
+    echo "${video_file}";
     if ! [ -f "${thumbnail_file}" ]
     then
-        #echo "Create ${thumbnail_file}";
-        ffmpegthumbnailer -i "${video_file}" -o "${thumbnail_file}";
+        echo "- create ${thumbnail_file}";
+        ffmpegthumbnailer -s 160 -i "${video_file}" -o "${thumbnail_file}";
     fi
 done;
