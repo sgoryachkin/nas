f6dbde06e06c9602d4ccb9900a6bffadba48213c
sgoryachkin <artful.tiger@gmail.com>
2017-06-05 23:21:30 +0300

    Update create-thumbnail.sh

--- create-thumbnail.sh
+++ create-thumbnail.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ffmpegthumbnailer -i "${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}" -o "${TR_TORRENT_DIR}/${TR_TORRENT_NAME}.jpeg"
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for video_file in $(find `pwd` -type f -iname "*.mov" -o -iname "*.avi" -o -iname "*.mkv");
+do
+    thumbnail_file=${video_file}.jpg;
+    #echo "${video_file}"
+    if ! [ -f "${thumbnail_file}" ]
+    then
+        #echo "Create ${thumbnail_file}";
+        ffmpegthumbnailer -i "${video_file}" -o "${thumbnail_file}";
+    fi
+done;
