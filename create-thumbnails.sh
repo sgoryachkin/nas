9ae9322da31a9ce87c2812a674aa953a7645cff3
sgoryachkin <artful.tiger@gmail.com>
2017-06-06 16:51:20 +0300

    Update create-thumbnails.sh

--- create-thumbnails.sh
+++ create-thumbnails.sh
@@ -3,6 +3,8 @@ media_dir="$( dirname "$0" )";
 echo "Media dir: $media_dir"
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+
+# Create Thumbnails
 for video_file in $(find "${media_dir}" -type f -iname "*.mov" -o -iname "*.avi" -o -iname "*.mkv");
 do
     echo "${video_file}";
@@ -13,14 +15,19 @@ do
         ffmpegthumbnailer -s 160 -i "${video_file}" -o "${thumbnail_file}";
     fi
 done;
+
 # Clean Up
 for thumbnail_file in $(find "${media_dir}" -type f -iname "*.mov.cover.jpg" -o -iname "*.avi.cover.jpg" -o -iname "*.mkv.cover.jpg");
 do
     echo "${thumbnail_file}";
-    video_file=${thumbnail_file}.cover.jpg;
+    video_file=${thumbnail_file%.cover.jpg};
+    echo "${video_file}";
     if ! [ -f "${video_file}" ]
     then
         echo "- remove ${thumbnail_file}";
         rm "${video_file}";
     fi
 done;
+
+# Clean Up Empty dirs
+find "${media_dir}" -type d -empty -delete
